5ae2e5fb5aecf68ab5ef11186348f44ca82eed10
oumaima BENCHRIF <ben.benchrifoumaima@gmail.com>
2023-08-07 15:45:46 +0200

    update agence css

--- resources/views/apps/agence.blade.php
+++ resources/views/apps/agence.blade.php
@@ -44,7 +44,7 @@
                                     <div class="widget-header">
                                         <div class="row">
                                             <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                                                <h4>{{__('Forms')}}Form Validation</h4>
+                                                <h4>Gestion Agence</h4>
                                             </div>
                                         </div>
                                     </div>
