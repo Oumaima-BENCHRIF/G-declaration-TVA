585c9a379bd27bd0922a7a56619e2c92c0168e75
oumaima BENCHRIF <ben.benchrifoumaima@gmail.com>
2023-08-07 14:37:34 +0200

    update footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Copyright Footer Starts -->
-<div class="footer-wrapper">
+<!-- <div class="footer-wrapper">
     <div class="footer-section f-section-1">
         <p class=""> {{ __('Copyright © '.date("Y").' NeptuneWeb. All rights reserved.')}} </p>
 
@@ -7,5 +7,5 @@
     <div class="footer-section f-section-2">
         <p class=""> {{ __('Crafted with extra')}}  <i class="las la-heart text-danger"></i></p>
     </div>
-</div>
+</div> -->
 <!-- Copyright Footer Ends -->
